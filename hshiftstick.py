759d38e4af4139d66176d69593370ab40ad492c8
Zed-z <71879051+Zed-z@users.noreply.github.com>
2022-11-06 22:40:55 +0100

    Revert "vJoy experiment"
    This reverts commit 25fcf6aa0307f8f0ea24596058ceef9467ac5781.

--- hshiftstick.py
+++ hshiftstick.py
@@ -19,8 +19,6 @@ pydirectinput.KEYBOARD_MAPPING['num7'] = 0x47
 pydirectinput.KEYBOARD_MAPPING['num8'] = 0x48
 pydirectinput.KEYBOARD_MAPPING['num9'] = 0x49
 
-import pyvjoy
-
 try:
     import tkinter as tk
 except ImportError:
@@ -252,12 +250,6 @@ def key_press(key):
     else:
         pyautogui.keyDown(key)
 
-    try:
-        vjoy = pyvjoy.VJoyDevice(1)
-        vjoy.set_button(int(key.replace("num", "")) + 1, 1)
-    except:
-        print("No vJoy")
-
     # print("started pressing", key)
 
 def key_release(key):
@@ -265,12 +257,6 @@ def key_release(key):
         pydirectinput.keyUp(key)
     else:
         pyautogui.keyUp(key)
-
-    try:
-        vjoy = pyvjoy.VJoyDevice(1)
-        vjoy.set_button(int(key.replace("num", "")) + 1, 0)
-    except:
-        print("No vJoy")
 
     # print("stopped pressing", key)
 
@@ -752,7 +738,7 @@ def main_loop():
     # Handle XInput events
     events = get_events()
     for event in events:
-        print("event")
+
         controller = controllers[event.user_index]
 
         if event.type == EVENT_CONNECTED:
